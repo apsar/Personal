877b697d7d3a5aa9dc2ebf6aec9d149349230803
apsar <apsard@xome.com>
2017-01-07 04:31:21 +0530

    First push

---
+++ Basics/sample.js
@@ -0,0 +1,14 @@
+let i = 5;
+for(i =0;i<10;i++)
+{
+  console.log('The value of i is '+i);
+}
+var color ='blue';
+switch(color)
+{
+  case 'white':
+  console.log('The color is '+color);
+  break;
+  case 'blue':
+  console.log(`The colors is ${color}`);
+}

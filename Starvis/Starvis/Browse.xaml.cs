872d2d7e38e9405a3d89bbca0c8c459a65e6a537
apsar <ApsarD@XMCHELAP030>
2017-03-17 17:42:59 +0530

    no message

--- Starvis/Starvis/Browse.xaml.cs
+++ Starvis/Starvis/Browse.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using StarvisDB;
 
 namespace Starvis
 {
@@ -23,6 +24,33 @@ namespace Starvis
         public Browse()
         {
             InitializeComponent();
+            PopulateGrid();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            var db = new Models();
+            string url = txtUrl.Text;
+            string text = txtText.Text;
+            string voice = txtVoice.Text;
+            var web = new WebDB { URL=url,TextCommand = text, VoiceCommand = voice };
+            db.WebDB.Add(web);
+            db.SaveChanges();
+            
+            ResetFields();
+        }
+        void ResetFields()
+        {
+            txtUrl.Text = string.Empty;
+            txtText.Text = string.Empty;
+            txtVoice.Text = string.Empty;
+        }
+        void PopulateGrid()
+        {
+            var db = new Models();
+            dataGrid.ItemsSource = db.WebDB.ToList();
+          
+            
         }
     }
 }
